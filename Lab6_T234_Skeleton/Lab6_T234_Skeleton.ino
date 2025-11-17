01ad6cf8a140237d214c0bbe46e8854dc01c60c4
Koil <wcwoxingle@outlook.com>
2025-11-17 20:08:05 +0800

    Final

--- Lab6_T234_Skeleton/Lab6_T234_Skeleton.ino
+++ Lab6_T234_Skeleton/Lab6_T234_Skeleton.ino
@@ -2,13 +2,13 @@
 
 //L298N Driver Pin 
 
-#define MOTOR_ENA ?  // Replace the ? with the GPIO pin you selected to connect ENA
-#define MOTOR_IN1 ?  // Replace the ? with the GPIO pin you selected to connect IN2
-#define MOTOR_IN2 ?  // Replace the ? with the GPIO pin you selected to connect IN2
+#define MOTOR_ENA 4  // Replace the ? with the GPIO pin you selected to connect ENA
+#define MOTOR_IN1 27  // Replace the ? with the GPIO pin you selected to connect IN2
+#define MOTOR_IN2 32  // Replace the ? with the GPIO pin you selected to connect IN2
 
 //Encoder Pin 
-#define ENCODER_PINA ? // Replace the ? with the GPIO pin you selected to connect encoder A
-#define ENCODER_PINB ? // Replace the ? with the GPIO pin you selected to connect encoder B
+#define ENCODER_PINA 34 // Replace the ? with the GPIO pin you selected to connect encoder A
+#define ENCODER_PINB 35 // Replace the ? with the GPIO pin you selected to connect encoder B
 
 //Encoder Counter
 volatile long encoderCount = 0; 
@@ -17,9 +17,9 @@ String command;
 
 //PID constants
 //** Modify these value for Task 2-4
-double kp = 0.1;
-double ki = 0.1;
-double kd = 0.1;
+double kp =0.48;
+double ki = 0.0003;
+double kd = 0.05;
 
 //PID Varibles 
 unsigned long currentTime, previousTime;
@@ -32,7 +32,7 @@ double cumError, rateError;
 // set desired position to 90 degrees
 double setPoint=90.0;
 
-int Task = 2 ; //Change this according to the task that you are doing 
+int Task = 4 ; //Change this according to the task that you are doing 
 
 void TaskConfig(){
   if(Task == 2){
@@ -88,7 +88,7 @@ void serialGraph(){
 // **Some value needs to be changed in order to use this
 double getPosition() {
   // Calculate the current position based on encoder count
-  position = float(encoderCount)*360.0/1000.0; // Replace 1000.0 with the actual counts per revolution
+  position = float(encoderCount)*360.0/194.0; // Replace 1000.0 with the actual counts per revolution
 
   if (position<0)
   {position = position + 360; // Ensure position is positive
@@ -100,20 +100,23 @@ double getPosition() {
 void setup() {
   
 /* pin mode for pins connected with L298N driver  */
-  ??? 
+  pinMode(MOTOR_IN1,OUTPUT);
+  pinMode(MOTOR_IN2,OUTPUT);
+  
 
 // encoder A pin mode for interrupt
   pinMode(ENCODER_PINA, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(ENCODER_PINA), encoderInterrupt, CHANGE);
 
 /*encoder B pin mode */   
-  ???
+  pinMode(ENCODER_PINB, INPUT_PULLUP);
+  
 
   //Configure PID value for different Task 
   TaskConfig(); 
 
 /* set up baud rate  */
-  ???
+  Serial.begin(115200);
 }
 
 void loop() {
@@ -124,19 +127,21 @@ void loop() {
         // control the motor based on PID output
         if(output<0){
            /*movement direction set when output<0 */
-           ???
+             digitalWrite(MOTOR_IN1,LOW);
+             digitalWrite(MOTOR_IN2,HIGH);
           }
           else{
             /*movement direction set when output>=0*/
-           ???
+              digitalWrite(MOTOR_IN1,HIGH);
+             digitalWrite(MOTOR_IN2,LOW);
           }
-        analogWrite(MOTOR_ENA,(128+int(abs(output))));   // Replace 128 with the the threshold value for your motor to move
+        analogWrite(MOTOR_ENA,(115)+abs(output));   // Replace 128 with the the threshold value for your motor to move
         
   //display position, setPoint and outout of controller in Serial Plotter
   serialGraph();
    // Reset encoder count
   if (position > 360 || position < 0) {
-    encoderCount = ?;
+    encoderCount = 0;
   } 
-delay(50);
+delay(15);
 }
